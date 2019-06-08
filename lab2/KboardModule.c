18ef2a7d1bb17096cd5726a482ea85470f745431
SuperNova911 <suwhan77@naver.com>
2019-06-08 23:12:05 +0900

    Readers Writers 문제 3가지 해결 방법 추가

--- lab2/KboardModule.c
+++ lab2/KboardModule.c
@@ -1,3 +1,4 @@
+#include <linux/delay.h>
 #include <linux/list.h>
 #include <linux/module.h>
 #include <linux/mutex.h>
@@ -8,6 +9,8 @@
 #include <linux/slab.h>
 #include <linux/uaccess.h>
 
+#define SYNC_SOLUTION 2
+
 #define KBOARD_DIRECTORY "kboard"
 #define KBOARD_WRITER "writer"
 #define KBOARD_READER "reader"
@@ -16,11 +19,20 @@
 #define RING_BUFFER_SIZE 5
 #define RING_BUFFER_INIT_VALUE -1
 #define WRITER_BUFFER_SIZE 20
+#define PERFORM_DELAY 3
 
 static inline void InitializeSemaphore(struct semaphore *sema, int value);
 
-static int InitializeKboard(void);
-static void DestroyKboard(void);
+static int InitializeProc(void);
+static void DestroyProc(void);
+
+static void InitializeKboard(void);
+
+static void InitializeSyncSolution(void);
+static void EnterCriticalSection_Writer(void);
+static void EnterCriticalSection_Reader(void);
+static void LeaveCriticalSection_Writer(void);
+static void LeaveCriticalSection_Reader(void);
 
 static int KboardWriter_Open(struct inode * inode, struct file * file);
 static int KboardWriter_Show(struct seq_file * file, void * unused);
@@ -31,8 +43,6 @@ static int KboardCounter_Open(struct inode * inode, struct file * file);
 static int KboardCounter_Show(struct seq_file * file, void * unused);
 static int KboardDumper_Open(struct inode * inode, struct file * file);
 static int KboardDumper_Show(struct seq_file * file, void * unused);
-
-static void KboardReset(void);
 
 static int __init KboardModuleInit(void);
 static void __exit KboardModuleExit(void);
@@ -78,17 +88,35 @@ static struct proc_dir_entry *KboardProcReader = NULL;
 static struct proc_dir_entry *KboardProcCounter = NULL;
 static struct proc_dir_entry *KboardProcDumper = NULL;
 
+#if SYNC_SOLUTION == 1
+static struct semaphore SemaphoreMutex;
+static struct semaphore SemaphoreWriter;
+static int ReaderCount;
+
+#elif SYNC_SOLUTION == 2
+static struct semaphore SemaphoreWriterMutex;
+static struct semaphore SemaphoreReaderMutex;
+static struct semaphore SemaphoreWriter;
+static struct semaphore SemaphoreReader;
+static int WriterCount;
+static int ReaderCount;
+
+#elif SYNC_SOLUTION == 3
 static struct semaphore SemaphoreMutex;
 static struct semaphore SemaphoreWriter;
 static struct semaphore SemaphoreReader;
+static int WriterCount;
 static int ReaderCount;
-
-static int PerformWriter;
-static int PerformReader;
+static int WriterWaitingCount;
+static int ReaderWaitingCount;
+#endif
 
 static int RingBuffer[RING_BUFFER_SIZE];
 static int RingBufferCount;
 static int RingBufferCurrentIndex;
+
+static int PerformWriter;
+static int PerformReader;
 
 static inline void InitializeSemaphore(struct semaphore *sema, int value)
 {
@@ -97,7 +125,7 @@ static inline void InitializeSemaphore(struct semaphore *sema, int value)
     lockdep_init_map(&sema->lock.dep_map, "semaphore->lock", &__key, 0);
 }
 
-static int InitializeKboard(void)
+static int InitializeProc(void)
 {
     printk(KERN_DEBUG "'%s'\n", __func__);
 
@@ -140,25 +168,15 @@ static int InitializeKboard(void)
         return -1;
     }
 
-    printk(KERN_ALERT "Created /proc/%s/%s\n", KBOARD_DIRECTORY, KBOARD_WRITER);
-    printk(KERN_ALERT "Created /proc/%s/%s\n", KBOARD_DIRECTORY, KBOARD_READER);
-    printk(KERN_ALERT "Created /proc/%s/%s\n", KBOARD_DIRECTORY, KBOARD_COUNTER);
-    printk(KERN_ALERT "Created /proc/%s/%s\n", KBOARD_DIRECTORY, KBOARD_DUMPER);
-
-    InitializeSemaphore(&SemaphoreMutex, 1);
-    InitializeSemaphore(&SemaphoreWriter, 1);
-    InitializeSemaphore(&SemaphoreReader, 6);
-    ReaderCount = 0;
-
-    PerformWriter = 0;
-    PerformWriter = 0;
-
-    KboardReset();
+    printk(KERN_DEBUG "Created /proc/%s/%s\n", KBOARD_DIRECTORY, KBOARD_WRITER);
+    printk(KERN_DEBUG "Created /proc/%s/%s\n", KBOARD_DIRECTORY, KBOARD_READER);
+    printk(KERN_DEBUG "Created /proc/%s/%s\n", KBOARD_DIRECTORY, KBOARD_COUNTER);
+    printk(KERN_DEBUG "Created /proc/%s/%s\n", KBOARD_DIRECTORY, KBOARD_DUMPER);
 
     return 0;
 }
 
-static void DestroyKboard(void)
+static void DestroyProc(void)
 {
     printk(KERN_DEBUG "'%s'\n", __func__);
 
@@ -169,10 +187,187 @@ static void DestroyKboard(void)
     proc_remove(KboardProcCounter);
     proc_remove(KboardProcDumper);
 
-    printk(KERN_ALERT "Removed /proc/%s/%s\n", KBOARD_DIRECTORY, KBOARD_WRITER);
-    printk(KERN_ALERT "Removed /proc/%s/%s\n", KBOARD_DIRECTORY, KBOARD_READER);
-    printk(KERN_ALERT "Removed /proc/%s/%s\n", KBOARD_DIRECTORY, KBOARD_COUNTER);
-    printk(KERN_ALERT "Removed /proc/%s/%s\n", KBOARD_DIRECTORY, KBOARD_DUMPER);
+    printk(KERN_DEBUG "Removed /proc/%s/%s\n", KBOARD_DIRECTORY, KBOARD_WRITER);
+    printk(KERN_DEBUG "Removed /proc/%s/%s\n", KBOARD_DIRECTORY, KBOARD_READER);
+    printk(KERN_DEBUG "Removed /proc/%s/%s\n", KBOARD_DIRECTORY, KBOARD_COUNTER);
+    printk(KERN_DEBUG "Removed /proc/%s/%s\n", KBOARD_DIRECTORY, KBOARD_DUMPER);
+}
+
+static void InitializeKboard(void)
+{
+    int index;
+
+    printk(KERN_DEBUG "'%s'\n", __func__);
+
+    for (index = 0; index < RING_BUFFER_SIZE; index++)
+    {
+        RingBuffer[index] = RING_BUFFER_INIT_VALUE;
+    }
+    RingBufferCount = 0;
+    RingBufferCurrentIndex = 0;
+    
+    PerformWriter = 0;
+    PerformReader = 0;
+}
+
+static void InitializeSyncSolution(void)
+{
+#if SYNC_SOLUTION == 1
+    InitializeSemaphore(&SemaphoreMutex, 1);
+    InitializeSemaphore(&SemaphoreWriter, 1);
+    ReaderCount = 0;
+
+#elif SYNC_SOLUTION == 2
+    InitializeSemaphore(&SemaphoreWriterMutex, 1);
+    InitializeSemaphore(&SemaphoreReaderMutex, 1);
+    InitializeSemaphore(&SemaphoreWriter, 1);
+    InitializeSemaphore(&SemaphoreReader, 1);
+    WriterCount = 0;
+    ReaderCount = 0;
+
+#elif SYNC_SOLUTION == 3
+    InitializeSemaphore(&SemaphoreMutex, 1);
+    InitializeSemaphore(&SemaphoreWriter, 0);
+    InitializeSemaphore(&SemaphoreReader, 0);
+    WriterCount = 0;
+    ReaderCount = 0;
+    WriterWaitingCount = 0;
+    ReaderWaitingCount = 0;
+#endif
+}
+
+static void EnterCriticalSection_Writer(void)
+{
+#if SYNC_SOLUTION == 1
+    down(&SemaphoreWriter);
+
+#elif SYNC_SOLUTION == 2
+    down(&SemaphoreWriterMutex);
+    WriterCount++;
+    if (WriterCount == 1)
+    {
+        down(&SemaphoreReader);
+    }
+    up(&SemaphoreWriterMutex);
+    down(&SemaphoreWriter);
+
+#elif SYNC_SOLUTION == 3
+    down(&SemaphoreMutex);
+    if (WriterCount > 0 || ReaderCount > 0 ||
+        WriterWaitingCount > 0 || ReaderWaitingCount > 0)
+    {
+        WriterWaitingCount++;
+        up(&SemaphoreMutex);
+        down(&SemaphoreWriter);
+        down(&SemaphoreMutex);
+        WriterWaitingCount--;
+    }
+    WriterCount++;
+    up(&SemaphoreMutex);
+#endif
+}
+
+static void EnterCriticalSection_Reader(void)
+{
+#if SYNC_SOLUTION == 1
+    down(&SemaphoreMutex);
+    ReaderCount++;
+    if (ReaderCount == 1)
+    {
+        down(&SemaphoreWriter);
+    }
+    up(&SemaphoreMutex);
+
+#elif SYNC_SOLUTION == 2
+    down(&SemaphoreReader);
+    up(&SemaphoreReader);
+    down(&SemaphoreReaderMutex);
+    ReaderCount++;
+    if (ReaderCount == 1)
+    {
+        down(&SemaphoreWriter);
+    }
+    up(&SemaphoreReaderMutex);
+
+#elif SYNC_SOLUTION == 3
+    down(&SemaphoreMutex);
+    if (WriterWaitingCount > 0 || WriterCount > 0)
+    {
+        ReaderWaitingCount++;
+        up(&SemaphoreMutex);
+        down(&SemaphoreReader);
+        down(&SemaphoreMutex);
+        ReaderWaitingCount--;
+    }
+    ReaderCount++;
+    up(&SemaphoreMutex);
+#endif
+}
+
+static void LeaveCriticalSection_Writer(void)
+{
+#if SYNC_SOLUTION == 1
+    up(&SemaphoreWriter);
+
+#elif SYNC_SOLUTION == 2
+    up(&SemaphoreWriter);
+    down(&SemaphoreWriterMutex);
+    WriterCount--;
+    if (WriterCount == 0)
+    {
+        up(&SemaphoreReader);
+    }
+    up(&SemaphoreWriterMutex);
+
+#elif SYNC_SOLUTION == 3
+    int index;
+
+    down(&SemaphoreMutex);
+    WriterCount--;
+    if (ReaderWaitingCount > 0)
+    {
+        for (index = 0; index < ReaderWaitingCount; index++)
+        {
+            up(&SemaphoreReader);
+        }
+    }
+    else if (WriterWaitingCount > 0)
+    {
+        up(&SemaphoreWriter);
+    }
+    up(&SemaphoreMutex);
+#endif
+}
+
+static void LeaveCriticalSection_Reader(void)
+{
+#if SYNC_SOLUTION == 1
+    down(&SemaphoreMutex);
+    ReaderCount--;
+    if (ReaderCount == 0)
+    {
+        up(&SemaphoreWriter);
+    }
+    up(&SemaphoreMutex);
+
+#elif SYNC_SOLUTION == 2
+    down(&SemaphoreReaderMutex);
+    ReaderCount--;
+    if (ReaderCount == 0)
+    {
+        up(&SemaphoreWriter);
+    }
+    up(&SemaphoreReaderMutex);
+
+#elif SYNC_SOLUTION == 3
+    down(&SemaphoreMutex);
+    ReaderCount--;
+    if (ReaderCount == 0 && WriterWaitingCount > 0)
+    {
+        up(&SemaphoreWriter);
+    }
+    up(&SemaphoreMutex);
+#endif
 }
 
 static int KboardWriter_Open(struct inode * inode, struct file * file)
@@ -187,13 +382,14 @@ static int KboardWriter_Show(struct seq_file * file, void * unused)
 
     printk(KERN_DEBUG "'%s'\n", __func__);
 
-    down(&SemaphoreWriter);
+    EnterCriticalSection_Writer();
+
     PerformWriter++;
 
     if (RingBufferCount <= 0)
     {
         printk("%s: Ring buffer is empty, count: '%d'\n", __func__, RingBufferCount);
-        up(&SemaphoreWriter);
+        LeaveCriticalSection_Writer();
         return -EPERM;
     }
 
@@ -202,7 +398,9 @@ static int KboardWriter_Show(struct seq_file * file, void * unused)
     RingBufferCount--;
     RingBufferCurrentIndex = (RingBufferCurrentIndex + 1) % RING_BUFFER_SIZE;
 
-    up(&SemaphoreWriter);
+	mdelay(PERFORM_DELAY);
+
+    LeaveCriticalSection_Writer();
 
     seq_printf(file, "Paste: '%d'\n", item);
 
@@ -242,21 +440,24 @@ static ssize_t KboardWriter_Write(struct file * file, const char __user * data, 
         return -EINVAL;
     }
 
-    down(&SemaphoreWriter);
+    EnterCriticalSection_Writer();
+
     PerformWriter++;
 
     if (RingBufferCount >= RING_BUFFER_SIZE)
     {
         printk(KERN_DEBUG "%s: Ring buffer is full, count: '%d'\n", __func__, RingBufferCount);
-        up(&SemaphoreWriter);
+        LeaveCriticalSection_Writer();
         return -EPERM;
     }
 
     RingBuffer[(RingBufferCurrentIndex + RingBufferCount) % RING_BUFFER_SIZE] = item;
     RingBufferCount++;
 
-    up(&SemaphoreWriter);
+	mdelay(PERFORM_DELAY);
     
+    LeaveCriticalSection_Writer();
+
     return length;
 }
 
@@ -268,6 +469,7 @@ static int KboardReader_Open(struct inode * inode, struct file * file)
 
 static int KboardReader_Show(struct seq_file * file, void * unused)
 {
+    int item;
     unsigned int randomIndex;
 
     printk(KERN_DEBUG "'%s'\n", __func__);
@@ -275,25 +477,17 @@ static int KboardReader_Show(struct seq_file * file, void * unused)
     get_random_bytes(&randomIndex, sizeof(randomIndex));
     randomIndex = randomIndex % RING_BUFFER_SIZE;
 
-    // down(&SemaphoreMutex);
-    ReaderCount++;
-    if (ReaderCount == 1)
-    {
-        // down(&SemaphoreWriter);
-    }
-    // up(&SemaphoreMutex);
+    EnterCriticalSection_Reader();
+
     PerformReader++;
+    item = RingBuffer[randomIndex];
+    
+	mdelay(PERFORM_DELAY);
+
+    LeaveCriticalSection_Reader();
 
     seq_printf(file, "Read random value from Kboard: index: '%d', value: '%d'\n",
-        randomIndex, RingBuffer[randomIndex]);
-
-    // down(&SemaphoreMutex);
-    ReaderCount--;
-    if (ReaderCount == 0)
-    {
-        // up(&SemaphoreWriter);
-    }
-    // up(&SemaphoreMutex);
+        randomIndex, item);
 
     return 0;
 }
@@ -332,35 +526,26 @@ static int KboardDumper_Show(struct seq_file * file, void * unused)
     seq_printf(file, "[Count: '%d']\n", RingBufferCount);
     seq_printf(file, "[CurrentIndex: '%d']\n", RingBufferCurrentIndex);
     seq_printf(file, "[Writer: '%d' times, Reader: '%d' times]\n", PerformWriter, PerformReader);
+    seq_printf(file, "[Synchronization Solution: '%d']\n", SYNC_SOLUTION);
     seq_printf(file, "===========================\n");
     
     return 0;
 }
 
-static void KboardReset(void)
-{
-    int index;
-
-    printk(KERN_DEBUG "'%s'\n", __func__);
-
-    for (index = 0; index < RING_BUFFER_SIZE; index++)
-    {
-        RingBuffer[index] = RING_BUFFER_INIT_VALUE;
-    }
-    RingBufferCount = 0;
-    RingBufferCurrentIndex = 0;
-}
-
 static int __init KboardModuleInit(void)
 {
     printk(KERN_DEBUG "'%s'\n", __func__);
-    return InitializeKboard();
+    
+    InitializeKboard();
+    InitializeSyncSolution();
+    return InitializeProc();
 }
 
 static void __exit KboardModuleExit(void)
 {
     printk(KERN_DEBUG "'%s'\n", __func__);
-    DestroyKboard();
+
+    DestroyProc();
 }
 
 module_init(KboardModuleInit);
